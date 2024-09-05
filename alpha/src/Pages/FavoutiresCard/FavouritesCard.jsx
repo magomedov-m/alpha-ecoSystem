b1a306cb329a3475bd283a4c687cf762f823cf20
magomedov-m <magomedov.dgu@gmail.com>
2024-09-05 06:52:04 +0300

    .

--- alpha/src/Pages/FavoutiresCard/FavouritesCard.jsx
+++ alpha/src/Pages/FavoutiresCard/FavouritesCard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import style from "./FavouritesCard.module.sass";
+import Card from "../../components/Card/Card";
+import ListStyle from '../../components/List/List.module.sass'
 import { useSelector } from "react-redux";
 
 export default function FavouritesCard() {
@@ -12,11 +14,19 @@ export default function FavouritesCard() {
           <img src="https://img.icons8.com/?size=50&id=cDx9hF2Mr4fv&format=png" />
         </a>
       </div>
-      <ul>
-        {favElem.map((m, i) => (
-          <li key={m.name}>{m.area}</li>
-        ))}
-      </ul>
+      <div className="country_like">
+      {favElem.map((movie) => (
+        <Card
+          movie={movie}
+          key={movie.name}
+          img={movie.flags[0]}
+          title={movie.name.official}
+          capital={movie.capital}
+          region={movie.region}
+          population={movie.population}
+        />
+      ))}
+      </div>
     </div>
   );
 }
